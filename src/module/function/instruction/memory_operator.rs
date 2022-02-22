de8da3f60b307f6ce83ae4e4e410435eb03aff28
einsJannis <einsjannis@gmail.com>
2022-02-22 15:58:11 +0100

    added load and store instructions

--- src/module/function/instruction/memory_operator.rs
+++ src/module/function/instruction/memory_operator.rs
@@ -1,6 +1,6 @@
 use std::fmt::Display;
 
-use crate::{types::first_class::Type, reference::Value};
+use crate::{types::{first_class::Type, ReturnType}, reference::Value};
 
 pub enum Instruction<'s> {
     AllocA(AllocAInstruction<'s>),
@@ -32,7 +32,104 @@ impl Display for AllocAInstruction<'_> {
     }
 }
 
-pub struct LoadInstruction<'s> {
+pub enum AtomicMemoryOrderingConstraint {
+    Unordered,
+    Monotonic,
+    Acquire,
+    Release,
+    AcquireRelease,
+    SequentiallyConsistent
+}
 
+pub enum LoadInstruction<'s> {
+    Normal {
+        volatile: bool,
+        value: Value<'s>,
+        alignment: usize,
+        //nontemporal: Option<Metadata>,
+        //invariant_load: Option<Metadata>,
+        //invariant_group: Option<Metadata>,
+        //nonnull: Option<Metadata>,
+        //dereferencable: Option<Metadata>,
+        //dereferencable_or_null: Option<Metadata>,
+        //align_m: Option<Metadata>,
+        //noundef: Option<Metadata>
+    },
+    Atomic {
+        volatile: bool,
+        value: Value<'s>,
+        syncscope: Option<&'s str>,
+        ordering: AtomicMemoryOrderingConstraint,
+        alignment: usize,
+        //invariant_group: Metadata,
+    }
+}
+
+impl Display for LoadInstruction<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str("load ")?;
+        match self {
+            Self::Normal { volatile, value, alignment } => {
+                if volatile { f.write_str("volatile ")?; }
+                let pointer_type = match value.return_type() {
+                    Type::SingleValue(crate::types::first_class::single_value::Type::Pointer(_type)) => _type,
+                    _ => panic!()
+                };
+                f.write_fmt(format_args!("{}, {}", return_type._type, value))?;
+                if alignment != 0 { f.write_fmt(", align {}", alignment)?; }
+            }
+            Self::Atomic { volatile, value, syncscope, ordering, alignment } => {
+                f.write_str("atomic ")?;
+                if volatile { f.write_str("volatile ")?; }
+                let pointer_type = match value.return_type() {
+                    Type::SingleValue(crate::types::first_class::single_value::Type::Pointer(_type)) => _type,
+                    _ => panic!()
+                };
+                f.write_fmt(format_args!("{}, {}", return_type._type, value))?;
+                if let Some(syncscope) = syncscope { f.write_fmt(" syncscope(\"{}\")", syncscope)?; }
+                f.write_fmt(format_args!(" {}, align {}", ordering, alignment))?;
+            }
+        }
+    }
+}
+
+pub enum StoreInstruction<'s> {
+    Normal {
+        volatile: bool,
+        value: Value<'s>,
+        pointer: Value<'s>,
+        alignment: usize,
+        //nontemporal: Metadata,
+        //invariant_group: Metadata,
+    },
+    Atomic {
+        volatile: bool,
+        value: Value<'s>,
+        pointer: Value<'s>,
+        syncscope: Option<&'s str>,
+        ordering: AtomicMemoryOrderingConstraint,
+        alignment: usize,
+        //invariant_group: Metadata,
+    }
+}
+
+impl Display for StoreInstruction<'s> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str("store ")?;
+        match self {
+            Self::Normal { volatile, value, pointer, alignment } => {
+                if volatile { f.write_str("volatile ")?; }
+                f.write_fmt(format_args!("{}, {}", value, pointer))?;
+                if alignment != 0 { f.write_fmt(", align {}", alignment)?; }
+            }
+            Self::Atomic { volatile, value, pointer, syncscope, ordering, alignment } => {
+                f.write_str("atomic ")?;
+                if volatile { f.write_str("volatile ")?; }
+                f.write_fmt(format_args!("{}, {}", value, pointer))?;
+                if let Some(syncscope) = syncscope { f.write_fmt(" syncscope(\"{}\")", syncscope)?; }
+                f.write_fmt(format_args!(" {}, align {}", ordering, alignment))?;
+            }
+        }
+    }
 }
 
